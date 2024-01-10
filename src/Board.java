e456146707ff8e6704fb333b3944e1d72c7806c6
Nabob <nabhanbd06@gmail.com>
2024-01-10 15:15:57 -0500

    Began creation of Board constructor, and the default medium piece counts

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 
 public class Board {
+
+    /**
+     * List of names for word files for the save info
+     * This save info will work similar to the system used by git
+     */
+    private String[] wordFileNames;
+
     /**
      * Stores the list of all valid words
      */
@@ -76,5 +83,20 @@ public class Board {
             {0,0,-2,0,0,0,0,0,-2,0,0},
             {0,-2,0,0,0,2,0,0,0,-2,0},
             {-2,0,0,0,2,0,2,0,0,0,-2}};
+
+    private static final int[] defaultMediumPieceCounts = {
+            9, 2, 2,
+    };
+
+    /**
+     * Creates a board from given information by player, all other information will be filled by program
+     * @param boardSize The size of the board, 0 is small, 1, is medium, 2 is large.
+     * @param wordFileNames Name of files from which to compile the word list. Do note the official word list must be explicitly included
+     * @param playerNames Name of players, the number of players is implicitly included
+     * @param pieceBagCounts The count of each piece, (e.g : 1 A, 5 B, 100 C...), -1 will take the default given by the board. 27 values A-Z plus blank count
+     */
+    public Board(int boardSize, String[] wordFileNames, String[] playerNames, String[] pieceBagCounts) {
+
+    }
 
 }
